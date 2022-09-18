850223f4f220de4ca59cb862782344d6dfa33e3d
immanuel <immanuelajimah@gmail.com>
2022-09-18 11:13:06 +0100

    Added input component to appliction

--- App.js
+++ App.js
@@ -1,121 +1,73 @@
-// import React from 'react';
-//  import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  Plaform,
+  StyleSheet,
+  View,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Text,
+} from 'react-native'
 
-//   const MainApp = () => {
+import SearchInput from './src/components/SearchInput'
+export default class App extends Component {
+  constructor(props) {
+    super(props)
 
-//     const name = '01- hey,this is my life';
+    this.state = {
+      location: 'Nigeria',
+    }
+  }
+  handleUpdateLocation = (city) => {
+    this.setState({
+      location: city,
+    })
+  }
+  render() {
+    return (
+      // <SearchInput
+      //   placeholder="search any city"
+      //   onSubmit={this.handleUpdateLocation}
+      // />
+      <KeyboardAvoidingView behaviour="padding" style={styles.container}>
+        <Text style={[styles.largeText, styles.textStyles]}>Nigeria</Text>
+        <Text style={[styles.smallText, styles.textStyles]}>light cloud</Text>
+        <Text style={[styles.largeText, styles.textStyles]}>
+          24 <Text>&#176;</Text>
+        </Text>
 
-//     return (
-//          <View style={styles.container}>
-//             <View style={styles.innerContainer} />
-//             <Text style={styles.title}>
-//               <Text style={styles.subtitle}> playing: </Text> {name}
-//             </Text>
-//          </View>
-//            )
-//          };
-
-//      const styles = StyleSheet.create({
-
-//        container: {
-//           margin:10,
-//           marginTop:100,
-//           backgroundColor: '#e67e22',
-//           borderRadius: 5,
-//          },
-
-//        innerContainer: {
-//          backgroundColor: '#d35400',
-//          height:50,
-//          width:140,
-//          borderTopLeftRadius: 5,
-//          borderBottomLeftRadius: 5,
-//          },
-
-//        title: {
-//          fontSize:18,
-//          fontWeight:'200',
-//          color:"#fff",
-//          position:'absolute',
-//         //  backgroundColor:'transparent',
-//          top: 12,
-//          left: 10,
-//          },
-
-//        subtitle: {
-//          fontWeight: "bold"
-//          },
-
-//        });
-
-//        export default MainApp;
-import React from 'react'
-import { StyleSheet, View, Image, ImageBackground } from 'react-native'
-
-const playIcon = require('./src/images/play.jpg')
-const volumeIcon = require('./src/images/sound.png')
-const hdIcon = require('./src/images/hd.png')
-const fullScreenIcon = require('./src/images/fullScreen.png')
-const remoteImage = {
-  uri: 'https://s3.amazonaws.com/crysfel/public/book/new-york.jpg',
+        <SearchInput placeholder="Search any city" />
+      </KeyboardAvoidingView>
+    )
+  }
 }
-
-const MainApp = () => {
-  return (
-    <ImageBackground source={remoteImage} style={styles.fullscreen}>
-      <View style={styles.container}>
-        <Image source={playIcon} style={styles.icon} />
-        <Image source={volumeIcon} style={styles.icon} />
-        <View style={styles.progressBar}>
-          <View style={styles.progressBar} />
-        </View>
-        <Image source={hdIcon} style={styles.con} />
-        <Image source={fullScreenIcon} style={styles.icon} />
-      </View>
-    </ImageBackground>
-  )
-}
-
 const styles = StyleSheet.create({
-  fullscreen: {
-    flex: 1,
-  },
   container: {
-    position: 'absolute',
-    backgroundColor: '#202020',
-    borderRadius: 5,
-    flexDirection: 'row',
-    height: 50,
-    padding: 5,
-    paddingTop: 16,
-    bottom: 30,
-    right: 10,
-    left: 10,
-    borderWidth: 1,
-    borderColor: '#303030',
-  },
-  icon: {
-    tintColor: '#fff',
-    height: 16,
-    width: 16,
-    marginLeft: 5,
-    marginRight: 5,
-  },
-  progress: {
-    backgroundColor: '#000',
-    borderRadius: 7,
     flex: 1,
-    height: 14,
-    margin: 10,
-    marginTop: 2,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  progressBar: {
-    backgroundColor: '#bf161c',
-    borderRadius: 5,
-    height: 10,
-    margin: 2,
-    width: 80,
+  textStyles: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
   },
 })
-
-export default MainApp
---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { StyleSheet, TextInput, View } from 'react-native'
+export default class SearchInput extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      // text: '',
+    }
+  }
+  handleChangeText = (text) => {
+    this.setState({ text })
+  }
+  // handleSubmitEditing = (text) => {
+  //   const { onSubmit } = this.props
+  //   const { text } = this.state
+
+  //   onSubmit(text)
+  //   this.setState({ text: '' })
+  // }
+  render() {
+    const { placeholder } = this.props
+    const { text } = this.state
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCorrect={false}
+          value={text}
+          placeholder={placeholder}
+          placeholderTextColor="white"
+          underlineColorAndriod="#ff0000"
+          style={styles.textInput}
+          clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          // onSubmitEditing={this.handleSubmitEditing}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    marginTop: 20,
+    backgroundColor: '#666',
+    marginHorizontal: 40,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+
+  textInput: {
+    flex: 1,
+    color: 'white',
+  },
+})
